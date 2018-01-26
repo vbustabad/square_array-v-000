a76ca1c812bcf1ce8e1345cc80cde8843dc781c2
Veronica Bustabad <vbustabad@gmail.com>
2018-01-26 16:44:28 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   array.each do |number|
-    number ** 2 = new_number
-    square_array << new_number
+    number ** 2 = squared_number
+    square_array << squared_number
   end
 end
